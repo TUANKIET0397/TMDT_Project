5ac9ef4d6c22a621d94f279d7e81a168094dd63b
TUANKIET0397 <kiet8103@gmail.com>
2025-11-18 16:44:55 +0700

    update addproduct in create

--- src/routes/site.js
+++ src/routes/site.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const siteController = require("../app/controllers/SiteController")
 
 // GET /site
-router.get("/checkout", siteController.checkout) //lúc làm đổi post
+router.get("/checkout", siteController.checkout) //lúc làm thêm post
 router.post("/payment", siteController.payment)
-router.get("/profile", siteController.profile) //lúc làm đổi post
+router.get("/profile", siteController.profile) //lúc làm thêm post
 router.get("/about", siteController.about)
 router.get("/", (req, res, next) => {
     siteController.index(req, res, next)
